52adff582f877db1a3bf1e2138a377f53a680384
ADZKA-AKN <muhammadadzkarullah@gmail.com>
2026-04-22 15:16:29 +0700

    new

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const mobileMenu = document.getElementById('mobile-menu');
 const closeMenu = document.getElementById('close-menu');
 const overlay = document.getElementById('menu-overlay');
 
-hamburger.addEventListener('click', ()=> {
+hamburger.addEventListener('click', () => {
     mobileMenu.classList.remove('-translate-x-full');
     overlay.classList.remove('hidden');
 });
@@ -15,3 +15,24 @@ const hideMenu = () => {
 
 closeMenu.addEventListener('click', hideMenu);
 overlay.addEventListener('click', hideMenu);
+
+function toggleFAQ(element) {
+    const allitems = document.querySelectorAll('.faq-items');
+    const content = element.querySelectorAll('.faq-content')
+    const img = element.querySelector('img');
+    const openIcon = 'assets/+.svg';
+    const closeIcon = 'assets/-.svg';
+    const isopen = !content.style.maxHeight;
+
+    allitems.forEach(item => {
+        const itemContent = item.querySelector('.faq-content');
+        const itemImg = item.querySelector('img');
+        itemContent.style.maxHeight = null;
+        itemImg.src = openIcon;
+    });
+
+    if (isopen) {
+        content.style.maxHeight = content.scrollHeight + 'px';
+        img.src = closeIcon;
+    }
+}
